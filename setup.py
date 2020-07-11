d0b4bbeccfd715ffa69f7e10e1a47ff665f09722
Jon Thielen <github@jont.cc>
2020-07-10 19:28:18 -0500

    Add basic project configuration and dummy test

---
+++ setup.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2020 OpenMosaic Developers.
+# Distributed under the terms of the Apache License, Version 2.0.
+# SPDX-License-Identifier: Apache-2.0
+"""Setup script for installing OpenMosaic."""
+
+from setuptools import setup
+
+setup(use_scm_version={'version_scheme': 'post-release'})
