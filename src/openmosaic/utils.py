9e69bbfd506b76f160daecc9379f949112ac37f5
Jon Thielen <github@jont.cc>
2020-11-24 10:41:54 -0600

    Switch from shared bucket use to hopefully faster client search

--- src/openmosaic/utils.py
+++ src/openmosaic/utils.py
@@ -3,10 +3,12 @@
 # SPDX-License-Identifier: Apache-2.0
 """Generic shared utils."""
 
+import logging
 import os
 import re
 import warnings
 
+import boto3
 import cftime
 import dask
 import geopandas as gpd
@@ -17,6 +19,7 @@ import shapely
 from tqdm import tqdm
 
 
+log = logging.getLogger(__name__)
 l2_datetime_pattern = re.compile(
     r"(?:K[A-Z]{3})(?P<Y>[0-9]{4})(?P<m>[0-9]{2})(?P<d>[0-9]{2})_(?P<H>[0-9]{2})"
     r"(?P<M>[0-9]{2})(?P<S>[0-9]{2})"
@@ -63,25 +66,26 @@ def filter_radar_sites(radar_sites, subbatch, r_max):
     return radar_sites[[radar_search_area.contains(p) for p in radar_sites['proj_geom']]]
 
 
-def create_s3_file_list(nexrad_bucket, radar_site_ids, analysis_time, vol_search_interval):
+def create_s3_file_list(radar_site_ids, analysis_time, vol_search_interval, bucket_name='noaa-nexrad-level2'):
     """Create listing of remote NEXRAD files of interest on S3 bucket.
 
     Parameters
     ----------
-    nexrad_bucket : boto3.resources.factory.s3.Bucket
-        NEXRAD Level II S3 Bucket object
     radar_site_ids : iterable
         Iterable of site NEXRAD site ids to query
     analysis_time : pandas.Timestamp
         Datetime of analysis
     vol_search_interval : pandas.Timedelta
         Maximum offset from analysis time for which to include files
+    bucket_name : str
+        Name of NEXRAD bucket on Amazon s3 (defaults to noaa-nexrad-level2)
     
     Returns
     -------
     list of str
         List of S3 file keys for Level II files of interest
     """
+    s3 = boto3.client('s3')
     dates_to_search = np.unique([
         t.floor('D') for t in [
             analysis_time - vol_search_interval,
@@ -93,9 +97,10 @@ def create_s3_file_list(nexrad_bucket, radar_site_ids, analysis_time, vol_search
     for date in dates_to_search:
         for site_id in radar_site_ids:
             all_files_on_day = [
-                obj.key for obj in nexrad_bucket.objects.filter(
+                obj['Key'] for obj in s3.list_objects_v2(
+                    Bucket=bucket_name,
                     Prefix=date.strftime(f"%Y/%m/%d/{site_id}")
-                )
+                )['Contents']
             ]
             for f in all_files_on_day:
                 match = l2_datetime_pattern.search(f)
@@ -134,46 +139,41 @@ def _load_single_site(f, cache_dir, analysis_time, sweep_interval):
         return None
 
 
-def download_nexrad_data(file_keys, nexrad_bucket, cache_dir):
-    """Download NEXRAD radar files from S3 bucket to a cache.
+def download_nexrad_data_serial(file_keys, cache_dir, bucket_name='noaa-nexrad-level2'):
+    """Download NEXRAD radar files from S3 bucket to a cache in serial fashion
     
     Parameters
     ----------
     file_keys : iterable of str
         S3 Bucket keys of Level II files of interest
-    nexrad_bucket : boto3.resources.factory.s3.Bucket
-        NEXRAD Level II S3 Bucket object
     cache_dir : str
         Directory for Level II file cache
-    analysis_time : pandas.Timestamp
-        Datetime of analysis
-    sweep_interval : pandas.Timedelta
-        Maximum offset from analysis time for which to include sweeps
+    bucket_name : str
+        Bucket name on S3 for nexrad data
 
     Returns
     -------
-    list of pyart.core.Radar
-        List of subsetted Radars
+    list of str
+        List of filepaths to l2 files
     """
+    s3 = boto3.client('s3')
     filepaths = []
     for k in file_keys:
         f = cache_dir + k.split("/")[-1]
         if not os.path.isfile(f):
-            warnings.warn(f"Downloading {f}")
-            nexrad_bucket.download_file(k, f)
+            log.info(f"Downloading {f}")
+            s3.download_file(bucket_name, k, f)
         filepaths.append(f)
     return filepaths
 
 
-def load_nexrad_data(file_keys, nexrad_bucket, cache_dir, analysis_time, sweep_interval):
-    """Load select sweeps of NEXRAD radar files into memory from S3 bucket using a cache.
+def load_nexrad_data_serial(file_keys, cache_dir, analysis_time, sweep_interval, bucket_name='noaa-nexrad-level2'):
+    """Load select sweeps of NEXRAD radar files into memory from S3 bucket using a cache in a serial fashion.
     
     Parameters
     ----------
     file_keys : iterable of str
         S3 Bucket keys of Level II files of interest
-    nexrad_bucket : boto3.resources.factory.s3.Bucket
-        NEXRAD Level II S3 Bucket object
     cache_dir : str
         Directory for Level II file cache
     analysis_time : pandas.Timestamp
@@ -186,11 +186,12 @@ def load_nexrad_data(file_keys, nexrad_bucket, cache_dir, analysis_time, sweep_i
     list of pyart.core.Radar
         List of subsetted Radars
     """
+    s3 = boto3.client('s3')
     radars = []
     for k in file_keys:
         f = cache_dir + k.split("/")[-1]
         if not os.path.isfile(f):
-            warnings.warn(f"Downloading {f}")
-            nexrad_bucket.download_file(k, f)
+            log.info(f"Downloading {f}")
+            s3.download_file(bucket_name, k, f)
         radars.append(_load_single_site(f, cache_dir, analysis_time, sweep_interval))
     return [radar for radar in dask.compute(*radars) if radar is not None]
