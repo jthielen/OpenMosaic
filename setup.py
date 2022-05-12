8dbe7ad38b60e49dafcf80e0b914c74b7101f0ee
jthielen <github@thielen.science>
2022-05-12 08:42:16 -0600

    Config changes for blue instead of black for formatting and no more cython

--- setup.py
+++ setup.py
@@ -3,32 +3,6 @@
 # SPDX-License-Identifier: Apache-2.0
 """Setup script for installing OpenMosaic."""
 
-from setuptools import Extension, setup
+from setuptools import setup
 
-setup(
-    use_scm_version={'version_scheme': 'post-release'},
-    ext_modules=[
-        Extension(
-            'openmosaic.gridding._gate_to_grid_map',
-            sources=[
-                'src/openmosaic/gridding/_gate_to_grid_map.pyx'
-            ],
-            libraries=['m']
-        ),
-        Extension(
-            'openmosaic.gridding._map_gates_to_subgrid',
-            sources=[
-                'src/openmosaic/gridding/_map_gates_to_subgrid.pyx'
-            ],
-            libraries=['m']
-        ),
-        Extension(
-            'openmosaic._c_routines',
-            sources=[
-                'src/openmosaic/_c_routines.pyx'
-            ],
-            libraries=['m']
-        )
-    ]
-)
-
+setup()
